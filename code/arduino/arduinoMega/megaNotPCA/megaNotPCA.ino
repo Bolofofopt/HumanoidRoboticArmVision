7ccf1f12fe991d759d376a238df98d6ab5abfe73
Henrique Abrantes <henriquesampayoabrantes@gmail.com>
2025-12-04 17:32:56 +0000

    Add Arduino servo and current measurement scripts
    Added megaNotPCA.ino for controlling servos via Arduino Mega and getCurrent.ino for measuring voltage and current using the INA219 sensor. These scripts support hardware control and monitoring for robotics and electronics projects.

---
+++ code/arduino/arduinoMega/megaNotPCA/megaNotPCA.ino
@@ -0,0 +1,91 @@
+#include <Wire.h>
+#include <Servo.h>
+
+const int PINOS_SERVOS[] = {2, 3, 4, 5, 6, 7};
+#define ID_COTOVELO 0
+#define ID_DEDO1    1 
+#define ID_DEDO2    2 
+#define ID_DEDO3    3 
+#define ID_DEDO4    4 
+#define ID_DEDO5    5
+
+Servo servos[6];
+
+const int ANGULO_ABERTO = 90;  // Equivalente ao seu antigo pulso de 90 graus
+const int ANGULO_FECHADO = 0;  // Equivalente ao seu antigo pulso de 0 graus
+
+// --- COMMS ---
+char g_bufferPC[100];    
+bool g_mensagemPronta = false; 
+int g_bufferIndex = 0;
+
+
+void processarMensagemRapida() {
+  // 1. Stepper (Uno)
+  char* token = strtok(g_bufferPC, ",");
+  if (token == NULL) return;
+  Serial2.write(token[0]); 
+
+  moverRapido(ID_COTOVELO);
+  moverRapido(ID_DEDO1);
+  moverRapido(ID_DEDO2);
+  moverRapido(ID_DEDO3);
+  moverRapido(ID_DEDO4);
+  moverRapido(ID_DEDO5);
+}
+
+inline void moverRapido(int idServo) {
+  char* token = strtok(NULL, ",");
+  if (token != NULL) {
+    // Se '1' vai para 90 graus, se não '0' vai para 0 graus
+    int angulo = (token[0] == '1') ? ANGULO_ABERTO : ANGULO_FECHADO;
+    
+    // Comando direto para a biblioteca Servo
+    servos[idServo].write(angulo);
+  }
+}
+
+void moverTodos(int ang) {
+  for (int i = 0; i < 6; i++) {
+    servos[i].write(ang);
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200); // Velocidade máxima na serial que comunica com o RaspberryPi
+  Serial2.begin(9600); // O Uno mantém-se a 9600
+
+  for(int i = 0; i < 6; i++) {
+    servos[i].attach(PINOS_SERVOS[i]);
+  }
+  delay(10);
+  
+  // Posição inicial rápida
+  moverTodos(0);
+  Serial.println("MEGA PRONTO (MODO TURBO 115200)");
+}
+
+void loop() {
+  while (Serial.available() > 0 && !g_mensagemPronta) {
+    char inChar = Serial.read();
+    if (inChar == '$') {
+      g_bufferIndex = 0;
+    } 
+    else if (inChar == '\n') {
+      g_bufferPC[g_bufferIndex] = '\0';
+      g_mensagemPronta = true;
+    } 
+    else if (g_bufferIndex < 99) {
+      g_bufferPC[g_bufferIndex] = inChar;
+      g_bufferIndex++;
+    }
+  }
+
+  if (g_mensagemPronta) {
+    processarMensagemRapida();
+    g_mensagemPronta = false;
+    g_bufferIndex = 0;
+  }
+}
+
